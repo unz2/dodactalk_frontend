a644a4ca115341f6afbe337bcc2db1679e821868
unz <y.heezoo@gmail.com>
2026-03-20 16:21:12 +0900

    vite-env 환경변수 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_KAKAO_REST_API_KEY: string
-  readonly VITE_KAKAO_REDIRECT_URI: string
+  readonly VITE_KAKAO_REST_API_KEY: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
+  readonly VITE_API_BASE_URL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
